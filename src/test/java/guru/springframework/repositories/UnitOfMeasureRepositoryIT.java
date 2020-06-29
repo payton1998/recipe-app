b8f8abf7b3db6de5fc9b4884f9a0bb9a99cc675e
Payton Mitchell <payton016@gmail.com>
2020-06-29 10:17:48 -0400

    implemented some j unit tests for controlers etc.

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,19 @@
+package guru.springframework.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitOfMeasureRepositoryIT {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindByDescription() {
+		fail("Not yet implemented");
+	}
+
+}
